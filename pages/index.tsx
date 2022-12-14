c3d02c3afd40f1dd2a1b68698795814ce0f2c908
Beksultan Kokpanbayev <b.kokpanbayev@mycar.kz>
2022-12-15 03:48:18 +0600

    feat: add forms

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,278 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import TextField from '@mui/material/TextField';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+
+import {useState} from "react";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
+    const genders = [
+        {
+            value: 1,
+            label: 'Male',
+        },
+        {
+            value: 0,
+            label: 'Female',
+        }
+    ];
+    const pains = [
+        {
+            value: 0,
+            label: 'Typical angina',
+        },
+        {
+            value: 1,
+            label: 'Atypical angina',
+        },
+        {
+            value: 2,
+            label: 'Non-anginal pain',
+        },
+        {
+            value: 3,
+            label: 'Asymtptomatic',
+        }
+    ];
+    const answers = [
+        {
+            value: 0,
+            label: 'No'
+        },
+        {
+            value: 1,
+            label: 'Yes'
+        }
+    ];
+    const slopes = [
+        {
+            value: 0,
+            label: 'Unslopping'
+        },
+        {
+            value: 1,
+            label: 'Downslopping'
+        }
+    ];
+    const levelsHemoglobin = [
+        {
+            value: 0,
+            label: 'I don\'t know'
+        },
+        {
+            value: 1,
+            label: 'Problem with heart'
+        },
+        {
+            value: 2,
+            label: 'No'
+        },
+        {
+            value: 3,
+            label: 'No normal blood flow'
+        },
+    ];
+
+    const [age, setAge] = useState<string>();
+    const [sex, setSex] = useState<string>();
+    const [cp, setCp] = useState<string>();
+    const [thalachh, setThalachh] = useState<string>();
+    const [exng, setExng] = useState<string>();
+    const [oldpeak, setOldpeak] = useState<string>();
+    const [slp, setSlp] = useState<string>();
+    const [caa, setCaa] = useState<string>();
+    const [thall, setThall] = useState<string>();
+
+    const [results, setResults] = useState();
+
+    const handleAgeChange = (event) => setAge(event.target.value);
+    const handleSexChange = (event) => setSex(event.target.value);
+    const handleCpChange = (event) => setCp(event.target.value);
+    const handleThalachhChange = (event) => setThalachh(event.target.value);
+    const handleExngChange = (event) => setExng(event.target.value);
+    const handleOldpeakChange = (event) => setOldpeak(event.target.value);
+    const handleSlpChange = (event) => setSlp(event.target.value);
+    const handleCaaChange = (event) => setCaa(event.target.value);
+    const handleThallChange = (event) => setThall(event.target.value);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const payload = {
+            "age": parseInt(age!),
+            "sex": parseInt(sex!),
+            "cp": parseInt(cp!),
+            "thalachh": parseInt(thalachh!),
+            "exng": parseInt(exng!),
+            "oldpeak": parseFloat(oldpeak!),
+            "slp": parseInt(slp!),
+            "caa": parseInt(caa!),
+            "thall": parseInt(thall!)
+        };
+
+        fetch("http://157.245.64.45:5000/predict", {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(payload),
+        }).then((res) => res.json())
+            .then((data) => setResults(data));
+    };
+
+  // @ts-ignore
+    // @ts-ignore
+    return (
+    <Container maxWidth="sm">
       <Head>
         <title>Hear attack prediction</title>
         <meta name="description" content="Heart attack application" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1>Heart attack prediction</h1>
+      <main>
+          <h1>Heart attack prediction</h1>
+          { !results ? (
+              <Box
+                  component="form"
+                  sx={{
+                      '& .MuiTextField-root': { m: 1, width: '25ch' },
+                  }}
+                  noValidate
+                  autoComplete="off"
+                  onSubmit={handleSubmit}
+              >
+                  <div>
+                      <TextField
+                          id="outlined-required"
+                          label="Age"
+                          type="number"
+                          inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+                          value={age}
+                          onChange={handleAgeChange}
+                      />
+                      <TextField
+                          id="outlined-selected"
+                          label="Gender"
+                          select
+                          SelectProps={{
+                              native: true,
+                          }}
+                          onChange={handleSexChange}
+                      >
+                          <option>Select</option>
+                          {genders.map((option) => (
+                              <option key={option.value} value={option.value}>
+                                  {option.label}
+                              </option>
+                          ))}
+                      </TextField>
+                      <TextField
+                          id="outlined-required"
+                          label="Chest pain"
+                          select
+                          SelectProps={{
+                              native: true,
+                          }}
+                          onChange={handleCpChange}
+                      >
+                          <option>Select</option>
+                          {pains.map((option) => (
+                              <option key={option.value} value={option.value}>
+                                  {option.label}
+                              </option>
+                          ))}
+                      </TextField>
+                      <TextField
+                          id="outlined-required"
+                          label="Maximum heart rate - (220 - age)"
+                          type="number"
+                          inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+                          value={thalachh}
+                          onChange={handleThalachhChange}
+                      />
+                      <TextField
+                          id="outlined-required"
+                          label="Your angina is result of exercise"
+                          select
+                          SelectProps={{
+                              native: true,
+                          }}
+                          onChange={handleExngChange}
+                      >
+                          <option>Select</option>
+                          {answers.map((option) => (
+                              <option key={option.value} value={option.value}>
+                                  {option.label}
+                              </option>
+                          ))}
+                      </TextField>
+                      <TextField
+                          id="outlined-required"
+                          label="ST depression"
+                          type="number"
+                          inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+                          value={oldpeak}
+                          onChange={handleOldpeakChange}
+                      />
+                      <TextField
+                          id="outlined-required"
+                          label="Slp"
+                          select
+                          SelectProps={{
+                              native: true,
+                          }}
+                          onChange={handleSlpChange}
+                      >
+                          <option>Select</option>
+                          {slopes.map((option) => (
+                              <option key={option.value} value={option.value}>
+                                  {option.label}
+                              </option>
+                          ))}
+                      </TextField>
+                      <TextField
+                          id="outlined-required"
+                          label="Caa"
+                          type="number"
+                          inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+                          value={caa}
+                          onChange={handleCaaChange}
+                      />
+                      <TextField
+                          id="outlined-required"
+                          label="Thall"
+                          select
+                          SelectProps={{
+                              native: true,
+                          }}
+                          onChange={handleThallChange}
+                      >
+                          <option>Select</option>
+                          {levelsHemoglobin.map((option) => (
+                              <option key={option.value} value={option.value}>
+                                  {option.label}
+                              </option>
+                          ))}
+                      </TextField>
+                  </div>
+                  <Button type="submit" variant="contained" fullWidth={true}>Submit</Button>
+              </Box>
+          ): (
+              <Stack
+                  direction="row"
+                  justifyContent="center"
+                  alignItems="center"
+                  spacing={2}
+              >
+                  <p>
+                      {results?.predict > 0 ? 'I\'m sorry to tell you, but you\'re at risk for a heart attack.  Please see a doctor if you have any complaints. Get well soon!'
+                      : 'Congratulations! You passed the rapid heart attack test. You are out of the risk zone for a heart attack. Stay healthy as you!' }
+                  </p>
+              </Stack>
+          )}
       </main>
 
-    
-    </div>
+
+    </Container>
   )
 }
